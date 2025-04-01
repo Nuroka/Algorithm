30452373aa8cbd103628b4537ce8f948aaba85cc
inhyuk <92858669+Nuroka@users.noreply.github.com>
2025-04-02 07:39:16 +0900

    [Silver IV] Title: 피보나치 비스무리한 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/14495. 피보나치 비스무리한 수열/피보나치 비스무리한 수열.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;
+    cin >> N;
+    vector<long long> v(N+1);
+
+    if (N == 1 || N == 2 || N == 3) {
+        cout << 1;
+        return 0;
+    }
+
+    v[1] = 1;
+    v[2] = 1;
+    v[3] = 1;
+
+    for (int i = 4; i <= N; i++) {
+        v[i] = v[i - 1] + v[i - 3];
+    }
+    
+    cout << v[N];
+
+    return 0;
+}
